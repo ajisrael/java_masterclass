993d72f741e3bf55ae62ecaf138bfc8140c99319
Alex Israels <aiuhjv1@amway.com>
2021-04-21 19:42:05 -0400

    F   Max and min int values

--- ByteShortIntLong/src/com/javamasterclass/Main.java
+++ ByteShortIntLong/src/com/javamasterclass/Main.java
@@ -3,6 +3,16 @@ package com.javamasterclass;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        int myValue = 10000;
+
+        int myMinIntValue = Integer.MIN_VALUE;
+        int myMaxIntValue = Integer.MAX_VALUE;
+        System.out.println("Integer Minimum Value = " + myMinIntValue);
+        System.out.println("Integer Maximum Value = " + myMaxIntValue);
+        System.out.println("Busted Min Value = " + (myMinIntValue - 1));
+        System.out.println("Busted MAX Value = " + (myMaxIntValue + 1));
+
+        int myMaxIntText = 2_147_483_647;
     }
 }
