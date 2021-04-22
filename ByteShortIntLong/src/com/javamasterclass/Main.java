37ef759b33b74fa51fc1d982fd9668ac3c410bfb
Alex Israels <aiuhjv1@amway.com>
2021-04-21 20:31:39 -0400

    F   intro to casting

--- ByteShortIntLong/src/com/javamasterclass/Main.java
+++ ByteShortIntLong/src/com/javamasterclass/Main.java
@@ -10,9 +10,35 @@ public class Main {
         int myMaxIntValue = Integer.MAX_VALUE;
         System.out.println("Integer Minimum Value = " + myMinIntValue);
         System.out.println("Integer Maximum Value = " + myMaxIntValue);
-        System.out.println("Busted Min Value = " + (myMinIntValue - 1));
-        System.out.println("Busted MAX Value = " + (myMaxIntValue + 1));
+        System.out.println("Busted MIN int Value = " + (myMinIntValue - 1));
+        System.out.println("Busted MAX int Value = " + (myMaxIntValue + 1));
 
         int myMaxIntText = 2_147_483_647;
+
+        byte myMinByteValue = Byte.MIN_VALUE;
+        byte myMaxByteValue = Byte.MAX_VALUE;
+        System.out.println("Byte Minimum Value = " + myMinByteValue);
+        System.out.println("Byte Maximum Value = " + myMaxByteValue);
+
+        short myMinShortValue = Short.MIN_VALUE;
+        short myMaxShortValue = Short.MAX_VALUE;
+        System.out.println("Short Minimum Value = " + myMinShortValue);
+        System.out.println("Short Maximum Value = " + myMaxShortValue);
+
+        long myLongValue = 100L;
+        long myMinLongValue = Long.MIN_VALUE;
+        long myMaxLongValue = Long.MAX_VALUE;
+        System.out.println("Long Minimum Value = " + myMinLongValue);
+        System.out.println("Long Maximum Value = " + myMaxLongValue);
+        long bigLongLiteralValue = 2_147_483_647_234L; // Need to have L
+        System.out.println(bigLongLiteralValue);
+
+        short bigShortLiteralValue = 32767;
+
+        int myTotal = (myMinIntValue / 2);
+
+        byte myNewByteValue = (byte) (myMinByteValue / 2);
+
+        short myNewShortValue = (short) (myMinShortValue / 2);
     }
 }
