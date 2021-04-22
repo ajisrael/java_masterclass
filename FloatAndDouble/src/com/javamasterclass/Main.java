59933c8db92f96b3e3e65dcda9ada16563fb6fa9
Alex Israels <aiuhjv1@amway.com>
2021-04-22 08:25:01 -0400

    F   floats and doubles

--- FloatAndDouble/src/com/javamasterclass/Main.java
+++ FloatAndDouble/src/com/javamasterclass/Main.java
@@ -3,6 +3,19 @@ package com.javamasterclass;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        float myMinFloatValue = Float.MIN_VALUE;
+        float myMaxFloatValue = Float.MAX_VALUE;
+        System.out.println("Float minimum value = " + myMinFloatValue);
+        System.out.println("Float maximum value = " + myMaxFloatValue);
+
+        double myMinDoubleValue = Double.MIN_VALUE;
+        double myMaxDoubleValue = Double.MAX_VALUE;
+        System.out.println("Double minimum value = " + myMinDoubleValue);
+        System.out.println("Double maximum value = " + myMaxDoubleValue);
+
+        int myInt = 5;
+        float myFloat = (float) 5.25; // double is default
+        double myDouble = 5.25d;
     }
 }
