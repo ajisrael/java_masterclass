b77dd4e6788aaaa73dfa37aa7e971cc482e94a85
Alex Israels <aiuhjv1@amway.com>
2021-04-28 09:49:41 -0400

    F   lbs to kg challenge

--- FloatAndDouble/src/com/javamasterclass/Main.java
+++ FloatAndDouble/src/com/javamasterclass/Main.java
@@ -14,8 +14,11 @@ public class Main {
         System.out.println("Double minimum value = " + myMinDoubleValue);
         System.out.println("Double maximum value = " + myMaxDoubleValue);
 
-        int myInt = 5;
-        float myFloat = (float) 5.25; // double is default
-        double myDouble = 5.25d;
+        int myInt = 5 / 3;
+        float myFloat = 5f / 3f;
+        double myDouble = 5d / 3d;
+        System.out.println("MyIntValue= " + myInt);
+        System.out.println("MyFloatValue= " + myFloat);
+        System.out.println("myDoubleValue= " + myDouble);
     }
 }
---
+++ PoundsToKilograms/src/com/javamasterclass/Main.java
@@ -0,0 +1,10 @@
+package com.javamasterclass;
+
+public class Main {
+
+    public static void main(String[] args) {
+        double pounds = 10.0d;
+        double kilograms = pounds * 0.45359237d;
+        System.out.println(pounds + " pounds = " + kilograms + " kilograms");
+    }
+}
