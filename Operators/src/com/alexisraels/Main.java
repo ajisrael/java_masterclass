a101199d3d0728df11235efd8405b0c17940a883
ajisrael <ajisrael@mtu.edu>
2021-05-06 11:42:21 -0400

    F   ternary operator

--- Operators/src/com/alexisraels/Main.java
+++ Operators/src/com/alexisraels/Main.java
@@ -80,5 +80,11 @@ public class Main {
             System.out.println("This is false");
         }
 
+        // Ternary Operator
+        isCar = true;
+        boolean wasCar = isCar ? true : false;
+        if (wasCar) {
+            System.out.println("wasCar is true");
+        }
     }
 }
