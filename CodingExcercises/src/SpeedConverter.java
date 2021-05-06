0a3fd82a04d99022a86a1f0c3496cd7638ab4001
ajisrael <ajisrael@mtu.edu>
2021-05-06 15:18:46 -0400

    F   completed speed converter challenge

---
+++ CodingExcercises/src/SpeedConverter.java
@@ -0,0 +1,20 @@
+public class SpeedConverter {
+    public static long toMilesPerHour(double kilometersPerHour) {
+        long milesPerHour = -1;
+
+        if (kilometersPerHour >= 0) {
+            milesPerHour = Math.round(kilometersPerHour / 1.609d);
+        }
+
+        return milesPerHour;
+    }
+
+    public static void printConversion (double kilometersPerHour) {
+        if (kilometersPerHour < 0) {
+            System.out.println("Invalid Value");
+        } else {
+            long milesPerHour = toMilesPerHour(kilometersPerHour);
+            System.out.println(kilometersPerHour + " km/h = " + milesPerHour + " mi/h");
+        }
+    }
+}
