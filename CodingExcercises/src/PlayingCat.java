fd10db80713e8bd7c7aba6063c31bc7ee43204dd
Alex Israels <aiuhjv1@amway.com>
2021-05-08 23:33:50 -0400

    F   completed is cat playing challenge

---
+++ CodingExcercises/src/PlayingCat.java
@@ -0,0 +1,7 @@
+public class PlayingCat {
+    public static boolean isCatPlaying(boolean summer, int temperature) {
+        int lowerLimit = 25;
+        int upperLimit = summer ? 45 : 35;
+        return (temperature >= lowerLimit && temperature <= upperLimit);
+    }
+}
