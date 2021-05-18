331de95febd73c67e3639bc6b941b62eb227805a
Alex Israels <alex.israels@amway.com>
2021-05-18 15:01:30 -0400

    F completed sum of 3 and 5 challenge

---
+++ SumOf3And5/src/com/alexisraels/Main.java
@@ -0,0 +1,20 @@
+package com.alexisraels;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int count = 0;
+        int sum = 0;
+        for (int i = 1; i <= 1000; i++) {
+            if ((i%3 == 0) && (i%5 == 0)) {
+                count++;
+                System.out.println(count + ": " + i);
+                sum += i;
+                if (count == 5) {
+                    break;
+                }
+            }
+        }
+        System.out.println("Sum: " + sum);
+    }
+}
